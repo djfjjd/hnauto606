0c08c3e2a0ebc126332c2d58d28d77ed4724de1d
djfjjd <djfjjd@djfjjdui-MacBookAir.local>
2026-08-28 10:20:08 +0900

    feat: simplify parking map header

---
+++ migrations/0003_rename_parking_zones.sql
@@ -0,0 +1,3 @@
+UPDATE parking_zones SET name='6층', short_name='6층', updated_at=CURRENT_TIMESTAMP WHERE id='pillar11';
+UPDATE parking_zones SET short_name='새싹', updated_at=CURRENT_TIMESTAMP WHERE id='tower';
+UPDATE parking_zones SET short_name='13층', updated_at=CURRENT_TIMESTAMP WHERE id='auto13';
