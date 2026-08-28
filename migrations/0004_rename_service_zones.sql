2080cdb68e49c8c67cfd63140190cb6e78ca060e
djfjjd <djfjjd@djfjjdui-MacBookAir.local>
2026-08-28 10:29:45 +0900

    feat: simplify service vehicle zones

---
+++ migrations/0004_rename_service_zones.sql
@@ -0,0 +1,3 @@
+UPDATE parking_zones SET name='광택', short_name='광택', updated_at=CURRENT_TIMESTAMP WHERE id='polish';
+UPDATE parking_zones SET name='판금', short_name='판금', updated_at=CURRENT_TIMESTAMP WHERE id='body';
+UPDATE parking_zones SET name='정비', short_name='정비', updated_at=CURRENT_TIMESTAMP WHERE id='repair';
